631efcb1b7b83bcca5be6fe59920c391874f6f89
025055 <xuhu01@chinatopcredit.com>
2019-07-13 22:43:29 +0800

    gradle install

--- cloud-consumer/src/main/java/com/xuhu/cloud/conf/RestConfig.java
+++ cloud-consumer/src/main/java/com/xuhu/cloud/conf/RestConfig.java
@@ -1,16 +1,13 @@
 package com.xuhu.cloud.conf;
 
-<<<<<<< HEAD
-=======
 import com.netflix.loadbalancer.IRule;
-import com.netflix.loadbalancer.RandomRule;
 import com.netflix.loadbalancer.RoundRobinRule;
-import org.springframework.boot.SpringBootConfiguration;
->>>>>>> a8689a617637afd6aa31d1ee83e100478e262c57
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+
 
 @Configuration
 public class RestConfig {
