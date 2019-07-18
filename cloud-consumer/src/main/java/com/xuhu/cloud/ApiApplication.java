9b8d0213d2c4ac0a1ee82e106e66bacb0def0bb9
025055 <xuhu01@chinatopcredit.com>
2019-07-18 14:07:13 +0800

    add port msg

--- cloud-consumer/src/main/java/com/xuhu/cloud/ApiApplication.java
+++ cloud-consumer/src/main/java/com/xuhu/cloud/ApiApplication.java
@@ -1,20 +1,31 @@
 package com.xuhu.cloud;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * EnableDiscoveryClient相比较EnableEurekaClient
  * 前者适用范围更广, 可在zookeeper consul等中使用
  */
+@RestController
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+    @GetMapping("/")
+    public String home() {
+        return "api running, port=" + port;
     }
 }
